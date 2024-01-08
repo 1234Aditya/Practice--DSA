23b1fdccb29a824eafdc9f9d8507b2fb9159ff01
Aditya Sethi <sethiaditya152@gmail.com>
2024-01-08 10:48:41 +0530

    equals

--- src/Strings/equals.java
+++ src/Strings/equals.java
@@ -2,6 +2,9 @@ package Strings;
 
 public class equals {
     public static void main(String[] args) {
-
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
+        System.out.print(s1==s2);
     }
 }
